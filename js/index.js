bef352c5ddbb3b3b67194aef526df82d9f1529fc
Ana Ferrandiz <anaferrandizpascual@gmail.com>
2024-07-26 11:02:08 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+// ************* MENÚ RESPONSIVE
+const headerBtn = document.querySelector('.header__button')
+const headerNav = document.querySelector('.header__nav')
+
+headerBtn.addEventListener('click', function(){
+    headerNav.classList.toggle('isActive')
+   })
+// ************* FIN MENÚ RESPONSIVE
